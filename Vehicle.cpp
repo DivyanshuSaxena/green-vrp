2742ba3252f48b608ff0fea30a9a288531a9de92
akshaypansari <akshay.pansari144@gmail.com>
2018-07-27 16:26:54 +0530

    Writing function for Adding Charging station or depot

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -5,3 +5,41 @@ using namespace std;
 bool Vehicle::timingCondition(Customer customer) {
     
 }
+bool functionAddingChargingStationOrDepot(Vehicle curr_Vehicle)
+{
+    double minCost=-1;
+    for(unsigned i=0; i<Customerpool.size();i++)
+    {
+        if(timingCondition(Customerpool[i]))
+        {
+            if (capacityCondition(Customerpool[i]))
+            {
+                if(chargingCondition(Customerpool[i]))
+                {
+                    //calculate distance
+                    //update minCOst
+                }
+                else
+                {
+                    //add charging station
+                    //update min
+                }
+            }
+            else
+            {
+                //add depot
+                //update min
+            }
+        }
+        if(minCost==-1)
+        {
+            //add to vehicle the customer with min cost
+        }
+        else
+        {
+            //return false which means depot need to be added and vehicle is retur
+        }
+
+
+    }
+}
