a88cacf5c055758443bae8f911accef495a2e5f5
akshaypansari <akshay.pansari144@gmail.com>
2018-07-27 17:34:33 +0530

    Edited the function Adding Charging Station

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -7,39 +7,64 @@ bool Vehicle::timingCondition(Customer customer) {
 }
 bool functionAddingChargingStationOrDepot(Vehicle curr_Vehicle)
 {
-    double minCost=-1;
     for(unsigned i=0; i<Customerpool.size();i++)
     {
+        double minCost=-1;
         if(timingCondition(Customerpool[i]))
         {
             if (capacityCondition(Customerpool[i]))
             {
-                if(chargingCondition(Customerpool[i]))
-                {
-                    //calculate distance
-                    //update minCOst
-                }
-                else
+                if(!chargingCondition(Customerpool[i]))//if charging condition is not satisfied
                 {
                     //add charging station
+                    curr_Vehicle.push_back(FindNearestChargingStation(i));
+                    //Find the nearest charging station
+                    //Then Calculate Cost
+                    CalculateCost(curr_Vehicle);
                     //update min
+                    if(currCost<minCost)
+                    {
+                        minCost=currCost;
+                        minID=i;
+                    }
+                curr_Vehicle.pop();//remove
+                    
+
+                }//this should not happen
+                else{
+                    cout<<"There is some Error";
+                    std::exit(0);
                 }
             }
             else
             {
-                //add depot
+                //Add depot
+                curr_Vehicle.push_back(mainDepotId);
+                //Calculate Cost
+                CalculateCost(Customerpool[i]));
+                if(currCost<minCost)
+                {                    
+                    minCost=currCost;
+                    minID=i;
+                }
+                curr_Vehicle.pop();//remove
+                // curr_Vehicle.add(Customerpool[i]);
+                
                 //update min
             }
         }
         if(minCost==-1)
         {
-            //add to vehicle the customer with min cost
+            curr_Vehicle.push_back(min_ID);
+            //update solutioncost??
+            return true;
         }
         else
         {
+            curr_Vehicle.push_back(mainDepotId));
+            //update solutioncost??
+            return true;
             //return false which means depot need to be added and vehicle is retur
         }
-
-
     }
 }
