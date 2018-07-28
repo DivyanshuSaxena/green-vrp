61b22f47252a69a7e88ab8f4cd3d1e1dee6a170d
akshaypansari <akshay.pansari144@gmail.com>
2018-07-28 23:24:37 +0530

    Wrote code for Input of file. Help me to convert string to time_type

--- Classes.h
+++ Classes.h
@@ -48,6 +48,7 @@ public:
 	int id;
 	double demandWeight;
 	double demandVol;
+	double lng,lat;
 	time_type timeWindowStarts;
 	time_type timeWindowEnds;
 };
@@ -69,6 +70,8 @@ double capVolType1, capVolType2;
 double capWeightType1, capWeightType2;
 double distanceType1, distanceType2;
 double fixedCost1, fixedCost2;
+double unitTransCost1, unitTransCost2;
+double chargeTime1, chargeTime2;
 
 // Miscellaneous
 time_type serviceTime;					// Service time for customers
@@ -77,6 +80,7 @@ time_type serviceTime;					// Service time for customers
 int waiting_factor;
 int charging_factor;
 
-vector<vector<double>> travelCosts;		// To be pre-processed based on the data form input
+// vector<vector<double>> travelCosts;		// To be pre-processed based on the data form input
+vector<vector<double>> travelDistance;		// To be pre-processed based on the data form input
 vector<vector<time_type>> travelTimes;	// To be input from the user
 vector<int> customerPool;				// The vector that will store the indexes of all Customers (in customers vector) that have not been added in any of the vehicles
--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "Classes.h"
 using namespace std;
 
@@ -57,7 +58,197 @@ int main() {
 // stoppingcondition--will be true if vehicle timing is over w.r.t. customers.//after that we will add new vehicle.
 
 void fillGlobalVariables() {
-	// Call certain file from here which will scan .xls(excel) file
+	// Call certain file from here which will scan .csv(excel) file
 	// and fill customerpool accordingly.
+
+	//reading vehicle type below
+	ifstream infile;
+	infile.open("input_vehicle_type.csv");
+	
+
+		string temp;
+		getline(infile,temp,'\n');
+
+	    getline(infile,temp,',');
+	    getline(infile,temp,',');
+	    getline(infile,temp,',');
+		capVolType1=stod(temp);
+	    getline(infile,temp,',');
+		capVolType1=stod(temp);	
+
+	    getline(infile,temp,',');
+
+	    getline(infile,temp,',');
+		distanceType1=stod(temp);
+		
+	    getline(infile,temp,',');
+		chargeTime1=stod(temp);
+		
+	    getline(infile,temp,',');
+		unitTransCost1=stod(temp);
+		
+	    getline(infile,temp,'\n');
+		fixedCost1=stod(temp);
+		
+
+	    getline(infile,temp,',');
+	    getline(infile,temp,',');
+	    getline(infile,temp,',');
+		capVolType2=stod(temp);
+	    getline(infile,temp,',');
+		capVolType2=stod(temp);	
+
+	    getline(infile,temp,',');
+
+	    getline(infile,temp,',');
+		distanceType2=stod(temp);
+		
+	    getline(infile,temp,',');
+		chargeTime2=stod(temp);
+		
+	    getline(infile,temp,',');
+		unitTransCost2=stod(temp);
+		
+	    getline(infile,temp,'\n');
+		fixedCost2=stod(temp);
+		//readingn vehicle info end
+		//input of distance_time begins
+
+		std::string file_path= "input_distance-time.txt";
+	//   std::cout<<"hi"<<std::endl;
+		std::ifstream fin(file_path.c_str());
+		// std::cout<<"hi"<<std::endl;
+		std::string temp;
+		getline(fin, temp);//ignoring the first line with heading
+		for(long i=0;i<=1211099;i++)
+		{
+			long fromNode,toNode;
+			getline(fin,temp,',');
+			fromNode=stoi(temp);
+			getline(fin,temp,',');
+			toNode=stoi(temp);
+			getline(fin,temp,',');
+
+			travelDistance[fromNode][toNode]=stod(temp);
+			getline(fin,temp,',');
+			travelTimes[fromNode][toNode]=stod(temp);
+		}
+		//distance-time input end between nodes
+		//reading the input_node file below
+		numChargingStations=100;
+		numCustomers=1000;
+		numNodes=1101;
+		ifstream infile;
+		infile.open("input_vehicle_type.csv");
+		string s_temp;
+		getline(infile,s_temp,'\n');//enter heading lines
+		//importing depot info
+
+		Customer depot;
+		getline(infile,s_temp,',');//id
+		depot.id=stoi(s_temp);
+		getline(infile,s_temp,',');//type
+		getline(infile,s_temp,',');//lng
+		// depot.lng=stod(s_temp);
+		getline(infile,s_temp,',');//lat
+		// depot.lat=stod(s_temp);
+		getline(infile,s_temp,',');
+		getline(infile,s_temp,',');
+		getline(infile,s_temp,',');//start time
+/*debug it*/		depot.timeWindowStarts=stoi(s_temp);
+		getline(infile,s_temp,'\n');//end time
+/*debug it*/		depot.timeWindowEnds=stoi(s_temp);
+		
+		customers.push_back(depot);
+		
+		
+		
+		
+		
+		
+		getline(infile,s_temp,',');
+		
+		for(int i=1;i<=1000;i++)
+		{
+			string s_temp;
+			Customer tempNode;
+			getline(infile,s_temp,',');//id
+			tempNode.id=stoi(s_temp);
+			getline(infile,s_temp,',');//type
+			getline(infile,s_temp,',');//lng
+			// tempNode.lng=stod(s_temp);
+			getline(infile,s_temp,',');//lat
+			// tempNode.lat=stod(s_temp);
+			getline(infile,s_temp,',');//weight
+			tempNode.demandWeight=stod(s_temp);
+			getline(infile,s_temp,',');//volume
+			tempNode.demandVol=stod(s_temp);			
+			getline(infile,s_temp,',');//start time
+	/*debug it*/		tempNode.timeWindowStarts=stoi(s_temp);
+			getline(infile,s_temp,'\n');//end time
+	/*debug it*/		tempNode.timeWindowEnds=stoi(s_temp);
+			
+			customers.push_back(tempNode);
+		}
+		for(int i=1001;i<=1100;i++)
+		{
+			string s_temp;
+			Customer csNode;
+			getline(infile,s_temp,',');//id
+			csNode.id=stoi(s_temp);
+			getline(infile,s_temp,',');//type
+			getline(infile,s_temp,',');//lng
+			// csNode.lng=stod(s_temp);
+			getline(infile,s_temp,',');//lat
+			// csNode.lat=stod(s_temp);
+			getline(infile,s_temp,',');//weight
+			getline(infile,s_temp,',');//volume
+			getline(infile,s_temp,',');//start time
+			getline(infile,s_temp,'\n');//end time
+			
+			customers.push_back(csNode);
+
+		}
+//reading input node end
+
+
+
+
+	    // getline(infile,capVolType1,',');
+
+		// getline(infile,capWeightType1,',');
+	    // getline(infile,temp,',');
+	    // getline(infile,distanceType1,',');
+	    // getline(infile,chargeTime1,',');
+	    // getline(infile,unitTransCost1,',');
+	    // getline(infile,fixedCost1,'\n');
+
+	    // getline(infile,temp,',');
+	    // getline(infile,temp,',');
+	    // getline(infile,capVolType2,',');
+	    // getline(infile,capWeightType2,',');
+	    // getline(infile,temp,',');
+	    // getline(infile,capVolType2,',');
+	    // getline(infile,chargeTime2,',');
+	    // getline(infile,unitTransCost2,',');
+	    // getline(infile,fixedCost2,'\n');
+		
+
+
+		
+		
+
+
+    vector<char*> values;
+    const int MAXSIZE = 4;
+    char thisVal[MAXSIZE];
+    while(in.getline(thisVal,MAXSIZE,',')) {
+        values.resize(values.size() + 1);
+        values.back() = new char[MAXSIZE];
+        strcpy(values.back(), thisVal);
+    }
+    in.close();
+    for (char* cp: values) cout<<*cp<<endl;
+
 
 }
